19b30d1728f058ac711ffae35eefbe297e389a0a
Damian <damianthesuski@gmail.com>
2016-01-06 16:25:43 -0500

    Initial Commit

---
+++ MasterMindMain.java
@@ -0,0 +1,25 @@
+import javax.swing.*;
+
+/**
+ * Created by Damian Suski on 1/6/2016.
+ */
+public class MasterMindMain extends JFrame {
+
+    public MasterMindMain(int height, int width)
+    {
+        setSize(height,width);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setResizable(false);
+        setVisible(true);
+        add(new Game());
+    }
+
+    public static void main(String args[])
+    {
+        //Run with different testing sizes from commandline
+        if(args.length==3)
+            new MasterMindMain(Integer.parseInt(args[1]),Integer.parseInt(args[2]));
+        else
+            new MasterMindMain(800,600);
+    }
+}
---
+++ entity.java
@@ -0,0 +1,13 @@
+import java.awt.*;
+
+/**
+ * Created by Damian Suski on 1/6/2016.
+ */
+public interface entity {
+
+    int getX();
+    int getY();
+    int getH();
+    int getW();
+    void draw(Graphics g);
+}
